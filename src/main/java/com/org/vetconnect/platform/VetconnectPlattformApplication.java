5143bafd98332fcf2f8d1447ea332eddcb4154b8
IsaacAmp24 <ampudiaflores@gmailcom>
2024-05-19 23:01:13 -0500

    initial commit.

---
+++ src/main/java/com/org/vetconnect/platform/VetconnectPlattformApplication.java
@@ -0,0 +1,13 @@
+package com.org.vetconnect.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetconnectPlattformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetconnectPlattformApplication.class, args);
+    }
+
+}
