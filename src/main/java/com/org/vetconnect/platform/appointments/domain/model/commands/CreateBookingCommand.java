1faaff19db5ca0f590f8a5d0fd2848af62a26763
IsaacAmp24 <ampudiaflores@gmailcom>
2024-06-05 13:40:11 -0500

    feat(appointments): queries for booking added.

--- src/main/java/com/org/vetconnect/platform/appointments/domain/model/commands/CreateBookingCommand.java
+++ src/main/java/com/org/vetconnect/platform/appointments/domain/model/commands/CreateBookingCommand.java
@@ -1,10 +1,13 @@
 package com.org.vetconnect.platform.appointments.domain.model.commands;
 
+import com.org.vetconnect.platform.appointments.domain.model.valueobjects.BookingDetails;
+import com.org.vetconnect.platform.appointments.domain.model.valueobjects.ServiceType;
+
 public record CreateBookingCommand(
         Long petOwnerId,
         Long vetCenterId,
-        String serviceType,
-        String bookingDetails,
+        ServiceType serviceType,
+        BookingDetails bookingDetails,
         Double price
 ) {
 }
