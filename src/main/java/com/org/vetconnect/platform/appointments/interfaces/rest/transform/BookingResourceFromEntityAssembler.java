f6568c3190ac8eb36df7cd1b729e1e2ba7b6de99
IsaacAmp24 <ampudiaflores@gmailcom>
2024-06-05 15:20:04 -0500

    feat(appointments): resources transform for bookings added

---
+++ src/main/java/com/org/vetconnect/platform/appointments/interfaces/rest/transform/BookingResourceFromEntityAssembler.java
@@ -0,0 +1,21 @@
+package com.org.vetconnect.platform.appointments.interfaces.rest.transform;
+
+import com.org.vetconnect.platform.appointments.domain.model.aggregates.Booking;
+import com.org.vetconnect.platform.appointments.interfaces.rest.resources.BookingResource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookingResourceFromEntityAssembler {
+    public static BookingResource toResourceFromEntity(Booking booking) {
+        return new BookingResource(
+                booking.getId(),
+                booking.getPetOwner().getId(),
+                booking.getVetCenter().getId(),
+                booking.getServiceType(),
+                booking.getBookingDetails(),
+                booking.getPrice(),
+                booking.getDate(),
+                booking.getDate()
+        );
+    }
+}
